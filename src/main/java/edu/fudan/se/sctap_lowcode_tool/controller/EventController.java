d95c66515427ec27c4ce97259cfe435e86e51a77
I.ChonFong <m124065624@gmail.com>
2024-04-28 20:11:14 +0800

    controller

--- src/main/java/edu/fudan/se/sctap_lowcode_tool/controller/EventController.java
+++ src/main/java/edu/fudan/se/sctap_lowcode_tool/controller/EventController.java
@@ -11,21 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "EventController", description = "事件控制器")
 public class EventController {
 
-    @Operation(summary = "Sctap前端发送当前事件给后端，之后应用构造的时候会用到")
-    @PostMapping("/event/post")
+    @Operation(summary = "Sctap前端发送当前事件给后端", description = "SCTAP以及融合感知模块也使用这个api， 之后应用构造的时候会用到")
+    @PostMapping("/event/history/post")
     public void postEvent() {
 
     }
 
 
-    @Operation(summary = "得到历史事件")
-    @GetMapping("/event/history")
+    @Operation(summary = "得到历史事件",description = "让SCTAP构造应用的时候需要此数据")
+    @GetMapping("/event/history/get")
     public void getEvent() {
 
     }
 
 
-    @Operation(summary = "构造好的应用，传给后端，然后后端让ms进行高亮显示，就相当于执行了构造的应用。前端是以怎样的形式传过来的呢")
+    @Operation(summary = "构造好的应用传给后端", description = "后端让ms进行高亮显示，就相当于执行了构造的应用")
     @PostMapping("/evnet/highlight")
     public void postEventHighlight() {
 
