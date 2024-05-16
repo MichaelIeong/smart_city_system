733e02cbf0bea3ad64778f9e106fa2cda8443a02
NanaChi187 <1870120101@qq.com>
2024-05-16 17:31:23 +0800

    增加了两个页面的入口

---
+++ src/main/resources/frontend/src/components/DeviceAccess.vue
@@ -0,0 +1,10 @@
+<!-- src/components/DeviceAccess.vue -->
+<template>
+  <div>
+    <h1>设备接入</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ src/main/resources/frontend/src/router/index.js
@@ -0,0 +1,28 @@
+// src/router/index.js
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from '@/components/Home.vue';
+import application from '@/components/application.vue';
+import DeviceAccess from '@/components/DeviceAccess.vue';
+
+Vue.use(Router);
+
+export default new Router({
+    routes: [
+        {
+            path: '/',
+            name: 'Home',
+            component: Home,
+        },
+        {
+            path: '/application',
+            name: 'application',
+            component: application,
+        },
+        {
+            path: '/device-access',
+            name: 'DeviceAccess',
+            component: DeviceAccess,
+        },
+    ],
+});
