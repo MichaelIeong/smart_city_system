69715dc62c6523c84abf993e59207358157b48e6
I.ChonFong <m124065624@gmail.com>
2024-05-22 16:38:21 +0800

    delete element.js

--- src/main/resources/static/src/App.vue
+++ src/main/resources/static/src/App.vue
@@ -1,28 +1,9 @@
-<!--<template>-->
-<!--  <div id="app">-->
-<!--    &lt;!&ndash; <HelloWorld msg="Welcome to Your Vue.js App" /> &ndash;&gt;-->
-<!--    <application></application>-->
-<!--  </div>-->
-<!--</template>-->
 
 <template>
   <div id="app">
     <router-view></router-view>
   </div>
 </template>
-
-<!--<script>-->
-<!--import HelloWorld from './components/HelloWorld.vue'-->
-<!--import application from './components/application.vue'-->
-
-<!--export default {-->
-<!--  name: 'app',-->
-<!--  components: {-->
-<!--    HelloWorld,-->
-<!--    application-->
-<!--  }-->
-<!--}-->
-<!--</script>-->
 
 <script>
 export default {
--- src/main/resources/static/src/main.js
+++ src/main/resources/static/src/main.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './plugins/element.js'
 import router from './router';
 import axios from 'axios';
+import Element from "element-ui";
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false;
 
 axios.defaults.baseURL = 'http://localhost:8080'
 Vue.prototype.$http = axios;
+
+Vue.use(Element)
+
 
 new Vue({
   router,
