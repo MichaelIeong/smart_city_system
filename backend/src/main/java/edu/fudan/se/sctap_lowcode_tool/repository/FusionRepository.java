1fa3c6e4279a99c934d8ae8f9c71c21575029768
sunlinyue <1870120101@qq.com>
2024-09-04 14:32:53 +0800

    之前没提交成功？烦死了*2

--- backend/src/main/java/edu/fudan/se/sctap_lowcode_tool/repository/FusionRepository.java
+++ backend/src/main/java/edu/fudan/se/sctap_lowcode_tool/repository/FusionRepository.java
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface FusionRepository extends JpaRepository<RuleInfo, Integer> {
 
-    @Override
-    <S extends RuleInfo> S save(S entity);
+//    @Override
+//    <S extends RuleInfo> S save(S entity);
 }
--- backend/src/main/java/edu/fudan/se/sctap_lowcode_tool/service/FusionService.java
+++ backend/src/main/java/edu/fudan/se/sctap_lowcode_tool/service/FusionService.java
@@ -2,7 +2,11 @@ package edu.fudan.se.sctap_lowcode_tool.service;
 
 import edu.fudan.se.sctap_lowcode_tool.model.RuleInfo;
 
+import java.util.List;
+
 public interface FusionService {
 
     boolean addNewRule(RuleInfo ruleInfo);
+
+    List<RuleInfo>  getRuleList();
 }
