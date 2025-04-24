3e72966dabc5d043466fc93f392198ea04d5c1d1
滕储华 <12647371+teng-cuhua@user.noreply.gitee.com>
2025-04-24 15:56:24 +0800

    add: 增加应用规则推荐和应用规则生成

--- backend/src/main/java/edu/fudan/se/sctap_lowcode_tool/service/AppRuleService.java
+++ backend/src/main/java/edu/fudan/se/sctap_lowcode_tool/service/AppRuleService.java
@@ -1,11 +1,14 @@
 package edu.fudan.se.sctap_lowcode_tool.service;
 
+import com.alibaba.dashscope.exception.NoApiKeyException;
 import edu.fudan.se.sctap_lowcode_tool.DTO.AppRuleRequest;
 import edu.fudan.se.sctap_lowcode_tool.DTO.BadRequestException;
 import edu.fudan.se.sctap_lowcode_tool.DTO.PageDTO;
 import edu.fudan.se.sctap_lowcode_tool.model.AppRuleInfo;
 import edu.fudan.se.sctap_lowcode_tool.repository.AppRuleRepository;
 import edu.fudan.se.sctap_lowcode_tool.repository.ProjectRepository;
+import edu.fudan.se.sctap_lowcode_tool.utils.milvus.MilvusUtil;
+import edu.fudan.se.sctap_lowcode_tool.utils.milvus.entity.AppRuleRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -23,6 +26,9 @@ public class AppRuleService {
     @Autowired
     private ProjectRepository projectRepository;
 
+    @Autowired
+    private MilvusUtil milvusUtil;
+
     public PageDTO<AppRuleInfo> getAllRulesByProjectId(Integer projectId, int pageNo, int pageSize) {
         PageRequest pageRequest = PageRequest.of(pageNo - 1, pageSize);
         Page<AppRuleInfo> repoResult = appRuleRepository.findAllByProjectId(projectId, pageRequest);
@@ -39,14 +45,21 @@ public class AppRuleService {
 
     public void deleteRulesByIds(Iterable<Integer> ruleIds) {
         appRuleRepository.deleteAllById(ruleIds);
+        // 从向量数据库中删除
+        for (Integer id : ruleIds) {
+            milvusUtil.deleteRecordById(id.toString());
+        }
     }
 
-    public void createRule(AppRuleRequest rule) {
+    public void createRule(AppRuleRequest rule) throws NoApiKeyException {
         var appRuleInfo = getEntityFromRequest(rule);
-        appRuleRepository.save(appRuleInfo);
+        appRuleInfo = appRuleRepository.save(appRuleInfo);
+        // 加入向量数据库
+        AppRuleRecord record = new AppRuleRecord(appRuleInfo.getId().toString(), rule.description());
+        milvusUtil.insertRecord(record);
     }
 
-    public void updateRule(Integer ruleId, AppRuleRequest rule) {
+    public void updateRule(Integer ruleId, AppRuleRequest rule) throws NoApiKeyException {
         var appRuleInfo = getEntityFromRequest(rule);
         if (appRuleRepository.findById(ruleId).isEmpty()) {
             throw new BadRequestException(
@@ -56,6 +69,10 @@ public class AppRuleService {
         }
         appRuleInfo.setId(ruleId);
         appRuleRepository.save(appRuleInfo);
+        // 更新向量数据库
+        milvusUtil.deleteRecordById(ruleId.toString());
+        AppRuleRecord record = new AppRuleRecord(ruleId.toString(), rule.description());
+        milvusUtil.insertRecord(record);
     }
 
     private AppRuleInfo getEntityFromRequest(AppRuleRequest rule) {
