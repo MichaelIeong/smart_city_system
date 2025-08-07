aa7b29725b4ac20f46427ac9c11d74f6801ed789
Chon-Fong,Ieong <m124065624@gmail.com>
2025-08-07 14:00:25 +0800

    fix: neo4j name=space_name

--- backend/src/main/java/edu/fudan/se/sctap_lowcode_tool/neo4jModel/SpaceNode.java
+++ backend/src/main/java/edu/fudan/se/sctap_lowcode_tool/neo4jModel/SpaceNode.java
@@ -2,6 +2,7 @@ package edu.fudan.se.sctap_lowcode_tool.neo4jModel;
 
 import org.springframework.data.neo4j.core.schema.*;
 import lombok.Data;
+import org.stringtemplate.v4.ST;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -13,6 +14,7 @@ public class SpaceNode {
     @Id
     private Integer spaceId;      // 作为主键，Project 内唯一
 
+    private String name;   // 空间名称（与 spaceName 相同）
     private String spaceName;    // 空间名称
     private String fixedProperties; // 固定属性（JSON字符串）
     private String description;  // 描述（如“卧室1”）
@@ -22,6 +24,11 @@ public class SpaceNode {
 
     @Relationship(type = "ADJACENT_TO", direction = Relationship.Direction.OUTGOING)
     private Set<SpaceNode> adjacentSpaces = new HashSet<>();
+
+    public void setSpaceName(String spaceName) {
+        this.spaceName = spaceName;
+        this.name = spaceName; // 自动同步
+    }
 
     public void addAdjacentSpace(SpaceNode space) {
         if (this.spaceId.equals(space.spaceId) || this.adjacentSpaces.contains(space)) {
