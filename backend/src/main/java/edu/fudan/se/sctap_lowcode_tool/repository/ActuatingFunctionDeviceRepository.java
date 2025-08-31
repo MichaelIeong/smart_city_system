3cff81807a921aacd0307ac05092f25211f94051
sunlinyue <1870120101@qq.com>
2025-09-01 02:33:43 +0800

    新版执行引擎

--- backend/src/main/java/edu/fudan/se/sctap_lowcode_tool/repository/ActuatingFunctionDeviceRepository.java
+++ backend/src/main/java/edu/fudan/se/sctap_lowcode_tool/repository/ActuatingFunctionDeviceRepository.java
@@ -1,10 +1,13 @@
 package edu.fudan.se.sctap_lowcode_tool.repository;
 
+import edu.fudan.se.sctap_lowcode_tool.model.ActuatingFunctionDevice;
 import edu.fudan.se.sctap_lowcode_tool.model.ActuatingFunctionInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ActuatingFunctionDeviceRepository extends JpaRepository<ActuatingFunctionInfo, Integer> {
@@ -12,4 +15,7 @@ public interface ActuatingFunctionDeviceRepository extends JpaRepository<Actuati
     // 使用 JPQL 查询 url 字段
     @Query("SELECT a.url FROM ActuatingFunctionDevice a WHERE a.actuatingFunction.id = :serviceId AND a.device.deviceName = :name")
     String findUrlByServiceIdAndName(@Param("serviceId") Integer serviceId, @Param("name") String name);
+
+    List<ActuatingFunctionDevice> findByDeviceId(Integer deviceId);
+
 }
