cf10ee19bcbe312c0652d28fc0634176f9fa1dfd
滕储华 <12647371+teng-cuhua@user.noreply.gitee.com>
2025-11-12 19:47:58 +0800

    fix: 修复跨域问题

--- backend/src/main/java/edu/fudan/se/sctap_lowcode_tool/config/WebConfig.java
+++ backend/src/main/java/edu/fudan/se/sctap_lowcode_tool/config/WebConfig.java
@@ -16,12 +16,10 @@ public class WebConfig {
                         .allowedOrigins(
                                 "http://127.0.0.1:8000",
                                 "http://localhost:8000",
-                                "http://10.177.29.134:8000",
-                                "http://10.177.29.61:8000",
+                                "http://10.176.65.202:8000",
                                 "http://127.0.0.1:1880",
                                 "http://localhost:1880",
-                                "http://10.177.29.134:1880",
-                                "http://10.177.29.61:1880"
+                                "http://10.176.65.202:1880"
                         )
                         .allowedMethods("*")
                         .allowedHeaders("*")
