68312637fc0465e47407fe3fea7e1e257e9c9d20
mizuki0xdc <1318252005@qq.com>
2026-01-14 10:42:35 +0800

    完成InfoService节点功能

--- backend/src/main/java/edu/fudan/se/sctap_lowcode_tool/controller/CyberResourceController.java
+++ backend/src/main/java/edu/fudan/se/sctap_lowcode_tool/controller/CyberResourceController.java
@@ -4,12 +4,10 @@ import edu.fudan.se.sctap_lowcode_tool.model.CyberResourceInfo;
 import edu.fudan.se.sctap_lowcode_tool.service.CyberResourceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/cyberResources")
@@ -22,6 +20,23 @@ public class CyberResourceController {
     public ResponseEntity<List<CyberResourceInfo>> getCyberResourcesByProjectId(@PathVariable Integer projectId) {
         System.out.println(cyberResourceService.getCyberResourceByProjectId(projectId));
         return ResponseEntity.ok(cyberResourceService.getCyberResourceByProjectId(projectId));
+    }
+
+    @GetMapping("/services")
+    public List<Map<String, String>> getTypes() {
+        return cyberResourceService.getCyberResource();
+    }
+
+    //根据服务名称获取对应的details
+    @GetMapping("/details")
+    public String getDetails(@RequestParam String description) {
+        return cyberResourceService.getMoreDetails(description);
+    }
+
+    //根据服务名称获取参数列表
+    @GetMapping("/params")
+    public String getParams(@RequestParam String description){
+        return cyberResourceService.getParamJson(description);
     }
 
 }
