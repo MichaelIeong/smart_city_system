0d6088e4679432d07554a07256b8c8ea834e36f8
Lin Yicheng <yichenglin20@fudan.edu.cn>
2026-01-23 14:09:09 +0800

    feat: 为融合引擎运行历史增加"是否成功"字段、增加获取运行历史的API接口、增加运行过程中的全局异常捕获

--- backend/src/main/java/edu/fudan/se/sctap_lowcode_tool/controller/EventFusionRuleController.java
+++ backend/src/main/java/edu/fudan/se/sctap_lowcode_tool/controller/EventFusionRuleController.java
@@ -1,9 +1,13 @@
 package edu.fudan.se.sctap_lowcode_tool.controller;
 
+import edu.fudan.se.sctap_lowcode_tool.DTO.BadRequestException;
+import edu.fudan.se.sctap_lowcode_tool.DTO.PageDTO;
 import edu.fudan.se.sctap_lowcode_tool.DTO.event_fusion_2026_jan.EventFusionRule;
 import edu.fudan.se.sctap_lowcode_tool.DTO.event_fusion_2026_jan.event.DataEvent;
 import edu.fudan.se.sctap_lowcode_tool.model.event_fusion_2026_jan.EventFusionRuleEntity;
+import edu.fudan.se.sctap_lowcode_tool.model.event_fusion_2026_jan.EventFusionRunHistory;
 import edu.fudan.se.sctap_lowcode_tool.service.event_fusion_2026_jan.EventFusionRuleService;
+import edu.fudan.se.sctap_lowcode_tool.service.event_fusion_2026_jan.EventFusionRunHistoryService;
 import edu.fudan.se.sctap_lowcode_tool.service.event_fusion_2026_jan.engine_component.EventIngestor;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -19,6 +23,7 @@ class EventFusionRuleController {
 
     private final EventFusionRuleService eventFusionRuleService;
     private final EventIngestor.DirectPushIngestor directPushIngestor;
+    private final EventFusionRunHistoryService historyService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -37,5 +42,20 @@ class EventFusionRuleController {
     @Tag(name = "模拟事件发生并推送事件到规则引擎流水线")
     public void pushEventToPipeline(@RequestBody DataEvent eventData) {
         directPushIngestor.push(eventData);
+    }
+
+    @GetMapping("/histories")
+    @Tag(name = "获取事件融合规则运行历史")
+    public PageDTO<EventFusionRunHistoryService.BriefResponse> getFusionRuleHistories(
+        @RequestParam(defaultValue = "0") int pageNum,
+        @RequestParam(defaultValue = "10") int pageSize
+    ) {
+        return historyService.getRunHistories(pageNum, pageSize);
+    }
+
+    @GetMapping("/histories/{id}")
+    @Tag(name = "获取事件融合规则运行历史详情")
+    public EventFusionRunHistory getFusionRuleHistoryDetail(@PathVariable Integer id) throws BadRequestException {
+        return historyService.getRunHistoryDetail(id);
     }
 }
